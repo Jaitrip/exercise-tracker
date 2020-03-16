5e07f4da1545c54024ba811ab20c578ae4557cee
Jaimik <JaimikT@outlook.com>
2020-03-16 14:36:42 +0000

    added endpoints for exercise and workouts

--- backend/endpoint_routes/ExerciseRoute.js
+++ backend/endpoint_routes/ExerciseRoute.js
@@ -1,3 +1,31 @@
 const exerciseRouter = require("express").Router();
 
+exerciseRouter.route("/addNewExercise").post((request, result) => {
+    const sqlQuery = "INSERT INTO Exercise (ExerciseID, Duration, CaloriesBurnt, WorkoutWorkoutID) VALUES (?)"
+    const values = [
+        request.body.exercise_id,
+        request.body.duration,
+        request.body.calories_burnt,
+        request.body.workout_id,
+    ]
+    connection.query(sqlQuery, [values], (error, res) => {
+        if (!error) {
+            result.send("Exercise Added")
+        } else {
+            result.send(error)
+        }
+    })
+})
+
+exerciseRouter.route("/getExerciseByWorkout").get((request, result) => {
+    const sqlQuery = "SELECT * FROM Exercise WHERE WorkoutWorkoutID = ?"
+    connection.query(sqlQuery, [request.params.workout_id], (error, rows, fields) => {
+        if (!error) {
+            result.send(rows)
+        } else {
+            result.send(error)
+        }
+    })
+})
+
 module.exports = exerciseRouter;
--- backend/endpoint_routes/WorkoutRoute.js
+++ backend/endpoint_routes/WorkoutRoute.js
@@ -1,3 +1,65 @@
 const workoutRouter = require("express").Router();
+const connection = require("../database.js");
+
+workoutRouter.route("/addNewWorkout").post((request, result) => {
+    const sqlQuery = "INSERT INTO Workout (WorkoutID, Name, Duration, Type, Date, UserUserID) VALUES (?)"
+    const values = [
+        request.body.workout_id,
+        request.body.name,
+        request.body.duration,
+        request.body.type,
+        request.body.date,
+        request.body.user_id
+    ]
+    connection.query(sqlQuery, [values], (error, res) => {
+        if (!error) {
+            result.send("Workout Added")
+        } else {
+            result.send(error)
+        }
+    })
+})
+
+workoutRouter.route("/getWorkoutByID").get((request, result) => {
+    const sqlQuery = "SELECT * FROM Workout WHERE WorkoutID = ?"
+    connection.query(sqlQuery, [request.params.workout_id], (error, rows, fields) => {
+        if (!error) {
+            result.send(rows)
+        } else {
+            result.send(error)
+        }
+    })
+})
+
+workoutRouter.route("/getWorkoutByDate").post((request, result) => {
+    const sqlQuery = "SELECT * FROM Workout WHERE UserUserID = ? AND Date = ?"
+    const values = [
+        request.body.user_id,
+        request.body.date
+    ]
+    connection.query(sqlQuery, [values], (error, rows, fields) => {
+        if (!error) {
+            result.send(rows)
+        } else {
+            result.send(error)
+        }
+    })
+})
+
+workoutRouter.route("/getWorkoutBetweenDates").post((request, result) => {
+    const sqlQuery = "SELECT * FROM Workout WHERE UserUserID = ? AND Date BETWEEN ? AND ?"
+    const values = [
+        request.body.user_id,
+        request.body.start_date,
+        request.body.end_date
+    ]
+    connection.query(sqlQuery, [values], (error, rows, fields) => {
+        if (!error) {
+            result.send(rows)
+        } else {
+            result.send(error)
+        }
+    })
+})
 
 module.exports = workoutRouter;
