74267abff857e05b5dc127218481ede5ace3bd8a
Jaimik <JaimikT@outlook.com>
2020-03-19 17:47:18 +0000

    added login

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../Login.css';
 
 export default class Login extends Component {
@@ -7,17 +8,50 @@ export default class Login extends Component {
 
         this.state = {
             username: '',
-            password: ''
+            password: '',
         }
+
+        this.onSubmit = this.onSubmit.bind(this)
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.onLoginSubmit = this.onLoginSubmit.bind(this)
     }
 
 
     onSubmit(e) {
         e.preventDefault();
-
-        // TODO CODE
-
         window.location = '/signup';
+    }
+
+    handleUsernameChange(event) {
+        this.setState({
+            username : event.target.value
+        })
+        event.preventDefault();
+    }
+
+    handlePasswordChange(event) {
+        this.setState({
+            password : event.target.value
+        })
+        event.preventDefault();
+    }
+
+    onLoginSubmit(event) {
+        axios.get("http://localhost:5000/user/getPassword/" + this.state.username)
+        .then(response => {
+            const actualPassword = response.data[0].password
+            if (actualPassword === this.state.password) {
+                window.location = "/weekly"
+            } else {
+                window.location = "/"
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            window.location = "/"
+        })     
+        event.preventDefault();      
     }
 
     render() {
@@ -25,10 +59,10 @@ export default class Login extends Component {
             <div>
                 <body id="login-page">
                     <div className="signin">
-                        <form>
+                        <form onSubmit={this.onLoginSubmit}>
                             <h2>Exercise Tracker</h2>
-                            <input type="text" placeholder="Enter Username"></input>
-                            <input type="password" placeholder="Enter Password"></input>
+                            <input type="text" placeholder="Enter Username" value={this.state.username} onChange={this.handleUsernameChange}></input>
+                            <input type="password" placeholder="Enter Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
                             <input type="submit" value="Log In"></input>
                         </form>
 
